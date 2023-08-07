75a9735479f3ac56e54e3f71193e85287ab51e6d
zidanegimiga <zidanegimigatemp@gmail.com>
2023-08-07 10:30:52 +0300

    add anim

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
-import Nav from '@/components/Nav/Nav'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+import Nav from "@/components/Nav/Nav";
+import { motion } from "framer-motion";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -20,17 +21,38 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Nav />
-        <div className={styles.saidiaTitle}>
+        <motion.div
+          initial={{
+            scale: 0,
+          }}
+          animate={{
+            scale: 1,
+          }}
+          transition={{ ease: "easeInOut", duration: 1 }}
+          className={styles.saidiaTitle}
+        >
           <img
             src={"/saidiaTitle.png"}
             alt={"Saidia Title"}
             className={styles.saidiaText}
           />
-        </div>
-        <img
+        </motion.div>
+        <motion.img
           src={"/kayaHead.png"}
           alt={"Saidia"}
           className={styles.saidiaHeadPic}
+          initial={{
+            y: 100,
+            scale: 0,
+          }}
+          animate={{
+            y: 0,
+            scale: 1,
+          }}
+          transition={{
+            ease: "easeInOut",
+            duration: 1,
+          }}
         />
       </main>
     </>
